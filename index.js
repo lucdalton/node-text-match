3f1980b3b7a6b4e56f0644c56021e34b55b693de
lucdalton <lucdalton@gmail.com>
2016-06-08 15:24:17 +0100

    added tests

---
+++ index.js
@@ -0,0 +1,144 @@
+/*
+	--------------------------------------------------
+	-- Simple text matching query language for node --
+	--------------------------------------------------
+*/
+
+var typeOptions = ["AND", "OR", "WITHIN", "NOT", "EXACT", "MATCH"];
+var uniaryOptions = ["NOT", "EXACT", "MATCH"];
+
+Array.prototype.indexOf = function(value) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] === value) {
+        return i;
+      }
+    }
+
+    return -1;
+}
+
+function checkQuery(query){
+
+	if(query.type && query.arg1){
+		if((uniaryOptions.indexOf(query.type) != -1) && (typeof query.arg1 == "string")){
+			return;
+		}
+	}
+
+	
+	if(typeof query !== "object"){
+		throw new Error('query type error');
+	};
+
+
+	if(!(query.arg1 && query.type)){
+		throw new Error('query type error');	
+	};
+
+	if(typeOptions.indexOf(query.type) == -1){
+		throw new Error('query type error');	
+	};
+
+	if(query.arg2){
+		checkQuery(query.arg2);
+	}
+
+	checkQuery(query.arg1);
+
+}
+
+
+function within(text, query){
+	// returns true or false
+	tokens = text.split(' ');
+	for(var i=0 ; i<tokens.length; i++){
+		if(tokens[i] == ''){
+			tokens.splice(i, 1);
+			i--;
+		}
+	}
+
+	for(var i=0; i<tokens.length; i++){
+		if(tokens[i] == query.arg1){
+			for(var j=0; j<tokens.length; j++){
+				if(tokens[j] == query.arg2){
+					var diff = j-i;
+					if(diff < 0){
+						diff *= -1;
+					};
+
+					if(diff <= query.within){
+						return true;
+					}
+
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function exact(text, word){
+	// returns true or false depending on if it's an exact match
+	var re = new RegExp(word);
+	return re.test(text);
+}
+
+function looseMatch(text, word){
+	// returns true or false depending on if it is a loose match
+	var re = new RegExp(word.toLowerCase());
+	return re.test(text.toLowerCase());
+
+}
+
+function notMatch(text, word){
+	// returns true or false depending on if it does not match
+	var re = new RegExp(word.toLowerCase());
+	return !re.test(text.toLowerCase());
+}
+
+
+
+
+function parseQuery(text, query){
+
+	if(query.type == "WITHIN"){
+		within(text, query);
+	};
+
+	if(query.arg2){
+
+		// it is a binary operator
+		if(query.type == "AND"){
+			return (parseQuery(text, query.arg1) && parseQuery(text, query.arg2));
+		};
+
+		if(query.type == "OR"){
+			return (parseQuery(text, query.arg1) || parseQuery(text, query.arg2));
+		};
+
+	}
+		
+	// else it is a uniary operator
+	if(query.type == "EXACT"){
+		return exact(text, query.arg1);
+	};
+
+	if(query.type == "MATCH"){
+		return looseMatch(text, query.arg1);
+	};
+
+	if(query.type == "NOT"){
+		return notMatch(text, query.arg1);
+	}
+
+}
+
+
+
+
+module.exports.parseQuery = parseQuery;
+module.exports.within = within;
+module.exports.exact = exact;
+module.exports.looseMatch = looseMatch;
+module.exports.notMatch = notMatch;
---
+++ test/index.js
@@ -0,0 +1,239 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+var parser = require('../index');
+
+
+var queryExample1 = {
+	type:"AND",
+	arg1:{
+		type:"OR",
+		arg1:{
+			type:"MATCH",
+			arg1:"compare"
+		},
+		arg2:{
+			type:"MATCH",
+			arg1:"market"
+		}
+	},
+
+
+	arg2:{
+		type:"MATCH",
+		arg1:"meerkat"
+	}
+
+};
+
+var withinExample1 = {
+	type:"WITHIN",
+	arg1:"compare",
+	arg2:"market",
+	within: 5
+}
+
+var withinExample2 = {
+	type:"WITHIN",
+	arg1:"compare",
+	arg2:"market",
+	within: 1
+}
+
+
+
+
+describe('within query function', function(){
+	
+	it('should return true', function(){
+		var testText = "the was compare ass teo fun market";
+		assert.equal(true, parser.within(testText, withinExample1));
+	});
+
+	it('should return false', function(){
+		var testText = "the was compare ass teo fun times shitty market";
+		assert.equal(false, parser.within(testText, withinExample1));
+	});
+
+
+	it('should return true', function(){
+		var testText = "the was market ass teo fun the compare";
+		assert.equal(true, parser.within(testText, withinExample1));
+	});
+
+	it('should return true', function(){
+		var testText = "market teo fun times shitty compare ass ";
+		assert.equal(true, parser.within(testText, withinExample1));
+	});
+
+
+	it('should return true', function(){
+		var testText = "the was compare market";
+		assert.equal(true, parser.within(testText, withinExample2));
+	});	
+
+	it('should return false', function(){
+		var testText = "the was compare a market";
+		assert.equal(false, parser.within(testText, withinExample2));
+	});	
+
+
+});
+
+
+var exactExample = {
+	type:"EXACT",
+	arg1:"CoMparE"
+
+}
+
+describe('exact query function', function(){
+
+	it('should return false', function(){
+		var testText = "the was compare market";
+		assert.equal(false, parser.exact(testText, exactExample.arg1));
+	});
+	
+	it('should return true', function(){
+		var testText = "the was CoMparE market";
+		assert.equal(true, parser.exact(testText, exactExample.arg1));
+	});
+
+	it('should be true', function(){
+		assert.equal(true, parser.exact("Compare the market is SHIT", "Compare"));
+	});
+
+});
+
+
+describe('match function', function(){
+
+	it('should return true', function(){
+		var testText = "the was CoMparE market";
+		assert.equal(true, parser.looseMatch(testText, exactExample.arg1));
+	})
+
+	it('should return true', function(){
+		var testText = "the was compare market";
+		assert.equal(true, parser.looseMatch(testText, exactExample.arg1));
+	})
+
+	it('should return true', function(){
+		var testText = "thewascomparemarket";
+		assert.equal(true, parser.looseMatch(testText, exactExample.arg1));
+	})
+
+
+})
+
+describe('notMatch function', function(){
+	
+	it('should return false', function(){
+		var testText = "the was compare market";
+		assert.equal(false, parser.notMatch(testText, exactExample.arg1));
+	})
+
+	it('should return true', function(){
+		var testText = "the was market";
+		assert.equal(true, parser.notMatch(testText, exactExample.arg1));
+	})
+
+
+})
+
+
+describe('integration tests', function(){
+
+	describe('parse multiple query', function(){
+
+		var t1 = "Compare the market is SHIT";
+		var t2 = "compare the market is SHIT";
+
+
+
+		var q1 = {
+			type:"WITHIN",
+			arg1:"compare",
+			arg2:"market",
+			within:3
+		};
+
+		var q2 = {
+			type:"EXACT",
+			arg1:"Compare",
+		};
+
+		var q3 = {
+			type:"MATCH",
+			arg1:"COMPARE"
+		};
+
+		var q4 = {
+			type:"MATCH",
+			arg1: "market"
+		};
+
+		var q5 = {
+			type:"AND",
+			arg1:q2,
+			arg2:q4
+		};
+
+		var q6 = {
+			type:"AND",
+			arg1:{
+				type:"MATCH",
+				arg1:"compare"
+			},
+			arg2:{
+				type:"MATCH",
+				arg1:"MARKET"
+			}
+		};
+
+		var q7 = {
+			type:"NOT",
+			arg1:"market"
+		}
+
+
+		it('parse test 1', function(){
+			assert.equal(true, parser.parseQuery(t1, q2));
+		});
+
+		it('parse test 2', function(){
+			assert.equal(true, parser.parseQuery(t1, q3));
+		});
+
+		it('parse test 3', function(){
+			assert.equal(true, parser.parseQuery(t2, q3));
+		});
+
+
+		it('AND should be true', function(){
+			assert.equal(true, parser.parseQuery(t1, q6));
+		})
+
+
+		it('AND should be true', function(){
+			assert.equal(true, parser.parseQuery(t1, q5));
+		})
+
+
+		it('MUTLI should be false', function(){
+			assert.equal(false, parser.parseQuery(t1, q7));
+		})
+
+
+
+	})
+
+
+	describe('execption tests', function(){
+
+	})
+
+
+
+})
+
